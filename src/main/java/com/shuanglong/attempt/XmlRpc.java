355cd2e23216ea9edd2f154cc3a91a80d4a093e3
xjshuanglong <xjshuanglong@126.com>
2019-11-07 13:12:43 +0800

    java xml-rpc python 传递 Double 数组测试

--- src/main/java/com/shuanglong/attempt/XmlRpc.java
+++ src/main/java/com/shuanglong/attempt/XmlRpc.java
@@ -3,6 +3,7 @@ package com.shuanglong.attempt;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Random;
 
 import org.apache.xmlrpc.XmlRpcException;
 import org.apache.xmlrpc.client.XmlRpcClient;
@@ -23,6 +24,37 @@ public class XmlRpc {
 			params.add("flyoung");
 			String result = (String) client.execute("get_string", params);
 			System.out.println("result:" + result);
+			
+			Random forceRandom = new Random();
+			ArrayList<Double[]> args = new ArrayList<>();
+			Double[] realtime_force = new Double[6];
+			for (int i=0; i<6; ++i)
+			{
+			    realtime_force[i] = forceRandom.nextDouble(); 
+			}
+			args.add(realtime_force);
+			Object setResult = client.execute("set_realtime_force", args);
+			System.out.println(setResult);
+			
+			ArrayList<Double[]> args2 = new ArrayList<>();
+			Object getResult = client.execute("get_realtime_force", args2);
+			if (getResult instanceof Object[])
+            {
+			    System.out.println("Bingo");
+			    Object[] resultArray = (Object[]) getResult;
+			    for (int i=0; i<resultArray.length; ++i)
+			    {
+			        System.out.println(resultArray[i]);
+//			        double temp = (double)resultArray[i];
+//			        System.out.println("Double: " + temp);
+			    }
+            }
+			else
+			{
+			    System.out.println("False");
+			    System.out.println(getResult);
+			}
+			
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		} catch (XmlRpcException e) {
--- src/main/resources/python/xml_rpc_server.py
+++ src/main/resources/python/xml_rpc_server.py
@@ -5,13 +5,27 @@ print("Hello World!")
 
 from xmlrpc.server import SimpleXMLRPCServer
 
+realtime_force_array = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+calibration_force_array = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+
 def respon_string(str):
     print("inside function: respon_string")
     return "get string:%s" % str
+    
+def set_realtime_force(realtime_force):
+    global realtime_force_array
+    realtime_force_array = realtime_force
+    print (realtime_force)
+    return "ok"
 
+def get_realtime_force():
+    return realtime_force_array
 
 if __name__ == '__main__':
     server = SimpleXMLRPCServer(('localhost', 8888))
     server.register_function(respon_string, "get_string")
+    server.register_function(set_realtime_force, "set_realtime_force")
+    server.register_function(get_realtime_force, "get_realtime_force")
     print ("Listening for Client")
     server.serve_forever()
+    
